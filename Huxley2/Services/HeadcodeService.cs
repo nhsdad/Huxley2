89691c3be50b17777175e33790e6600742479cf3
nhsdad <nhsdad@techadr.co.uk>
2026-06-06 18:32:16 +0100

    Update HeadcodeService.cs

--- Huxley2/Services/HeadcodeService.cs
+++ Huxley2/Services/HeadcodeService.cs
@@ -63,10 +63,14 @@ namespace Huxley2.Services
             }
  
             var result0 = await FetchHeadcodesAsync(crs, timeOffset);
+            var result15 = await FetchHeadcodesAsync(crs, timeOffset + 15);
+            var result30 = await FetchHeadcodesAsync(crs, timeOffset + 30);
             var result60 = await FetchHeadcodesAsync(crs, timeOffset + 60);
             var result120 = await FetchHeadcodesAsync(crs, timeOffset + 120);
 
             var result = new Dictionary<string, string>(result0);
+            foreach (var kv in result15) result.TryAdd(kv.Key, kv.Value);
+            foreach (var kv in result30) result.TryAdd(kv.Key, kv.Value);
             foreach (var kv in result60) result.TryAdd(kv.Key, kv.Value);
             foreach (var kv in result120) result.TryAdd(kv.Key, kv.Value);
  
