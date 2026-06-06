036ba9565a2ce5c9dd197133349b15fce1f76511
nhsdad <nhsdad@techadr.co.uk>
2026-06-06 16:36:20 +0100

    Update StationBoardService.cs

--- Huxley2/Services/StationBoardService.cs
+++ Huxley2/Services/StationBoardService.cs
@@ -47,7 +47,6 @@ namespace Huxley2.Services
                         var destCrs = service.destination?.Length > 0 ? service.destination[0]?.crs : null;
                         if (destCrs == null) continue;
                         var key = $"{service.std}|{destCrs}";
-                        _logger.LogWarning($"StationBoardService: looking up key '{key}'");
                         if (headcodes.TryGetValue(key, out var trainid))
                         {
                             service.trainid = trainid;
